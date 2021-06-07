6feb488fec6b08d70bd7c7416084ee90c6eb690f
ESLLO <master@esllo.com>
2021-06-08 04:18:17 +0900

    place time select update

--- src/pages/Place.js
+++ src/pages/Place.js
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/control-has-associated-label */
 import React, {
   forwardRef, useCallback, useEffect, useState,
@@ -27,25 +29,81 @@ const Place = () => {
   const iter = [0, 1, 2, 3];
   const [upCoord, setUpCoord] = useState(upperCoords);
   const [udCoord, setUdCoord] = useState(underCoords);
-  const [selected, setSelected] = useState(-1);
+  const [selected, setSelected] = useState(0);
+  const [size, setSize] = useState(false);
 
   const [date, setDate] = useState(new Date());
   registerLocale('ko', ko);
 
+  const rooms = ['대강의실', '스튜디오', '회의실', '실습실', '멘토링실1', '멘토링실2', '멘토링실3', '멘토링실4'];
+
+  const [times, setTimes] = useState([]);
+  const timeTables = [
+    '09:00 ~ 10:00',
+    '10:00 ~ 11:00',
+    '11:00 ~ 12:00',
+    '12:00 ~ 13:00',
+    '13:00 ~ 14:00',
+    '14:00 ~ 15:00',
+    '15:00 ~ 16:00',
+    '16:00 ~ 17:00',
+    '17:00 ~ 18:00'];
+
   const handleResize = useCallback(() => {
-    const w1 = document.querySelector('.place01').width;
-    const w2 = document.querySelector('.place02').width;
+    let w1 = 0;
+    let w2 = 0;
+    if (document.querySelector('.place01')) {
+      w1 = document.querySelector('.place01').width;
+      w2 = document.querySelector('.place02').width;
+    }
     const r1 = w1 / upper[0];
     const r2 = w2 / under[0];
     const upc = upperCoords.map(c => c * r1);
     const udc = underCoords.map(c => c * r2);
     setUpCoord(upc);
     setUdCoord(udc);
+    setSize(true);
   }, [under, underCoords, upper, upperCoords]);
 
   const handleClick = useCallback(function _() {
     setSelected(this);
   }, []);
+
+  const handleTimeClick = useCallback(function _() {
+    const _time = `t${this}`;
+    if (times.indexOf(_time) !== -1) {
+      const filtered = times.filter(time => time !== _time);
+      const nums = filtered.map(e => parseInt(e.substr(1), 10));
+      let remove = false;
+      for (let i = 0; i < nums.length - 1; i += 1) {
+        if (nums[i + 1] - nums[i] > 1) {
+          remove = i + 1;
+          break;
+        }
+      }
+      if (remove) {
+        filtered.splice(remove, filtered.length - remove);
+      }
+      setTimes(filtered);
+    } else {
+      if (times.length > 2) {
+        // 3시간 이상 불가
+        alert('3시간 이상 선택할 수 없습니다');
+        return;
+      }
+      const nums = times.map(e => parseInt(e.substr(1), 10));
+      nums.push(this);
+      nums.sort();
+      for (let i = 0; i < nums.length - 1; i += 1) {
+        if (nums[i + 1] - nums[i] > 1) {
+          // 시간 스킵 불가
+          alert('연속된 시간만 선택이 가능합니다.');
+          return;
+        }
+      }
+      setTimes([...times, _time].sort());
+    }
+  }, [times]);
 
   useEffect(() => {
     window.onload = handleResize;
@@ -64,6 +122,7 @@ const Place = () => {
               <div>
                 <img name="upper-img" className="place01" src="./images/upper.png" alt="place01" />
                 <div className="map" id="upper-img" name="upper-img">
+                  {!size && setTimeout(handleResize, 100) && ''}
                   {iter.map(i => {
                     const coords = upCoord.slice(i * 4, (i + 1) * 4);
                     const style = { left: `${coords[0]}px`, top: `${coords[1]}px` };
@@ -98,7 +157,7 @@ const Place = () => {
           </div>
           <div className="place-time">
             <div className="time-title">
-              <h2>멘토링실 3</h2>
+              <h2>{rooms[selected]}</h2>
             </div>
             <div className="time-wrap">
               <div className="time-date">
@@ -112,17 +171,9 @@ const Place = () => {
                   onChange={_date => setDate(_date)}
                 />
               </div>
-              <ul className="time-table">
-                <li>09:00 ~ 10:00</li>
-                <li>10:00 ~ 11:00</li>
-                <li>11:00 ~ 12:00</li>
-                <li>12:00 ~ 13:00</li>
-                <li>13:00 ~ 14:00</li>
-                <li>14:00 ~ 15:00</li>
-                <li>15:00 ~ 16:00</li>
-                <li>16:00 ~ 17:00</li>
-                <li>17:00 ~ 18:00</li>
-              </ul>
+              <div className="time-table">
+                {timeTables.map((_time, i) => <li className={times.indexOf(`t${i}`) !== -1 ? 'time-active' : ''} onClick={handleTimeClick.bind(i)} key={_time}>{_time}</li>)}
+              </div>
               <p className="time-alert">* 최대 이용시간은 3시간 입니다.</p>
             </div>
           </div>
